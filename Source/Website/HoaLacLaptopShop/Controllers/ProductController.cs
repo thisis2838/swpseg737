c17d7ed8d943af5456149f3bd3c5a41c87c28e35
LeKheing <lekhangit1106@gmail.com>
2024-05-18 22:47:05 +0700

    fix error product list

--- Source/Website/HoaLacLaptopShop/Controllers/ProductController.cs
+++ Source/Website/HoaLacLaptopShop/Controllers/ProductController.cs
@@ -22,12 +22,35 @@ namespace HoaLacLaptopShop.Controllers
 
             var rs = products.Select(p => new ProductVM
             {
+                Id = p.ID,
                 Name = p.Name,
                 Description = p.Description ?? "",
-                Link = _db.ProductImages.Where(pi => pi.ProductId == p.ID).FirstOrDefault().Link ?? "",
+                Link = _db.ProductImages.Where(pi => pi.ProductId == p.ID).Select(pi => pi.Link).ToList(),
                 Price = p.Price,
             });
             return View(rs);
+        }
+
+        public IActionResult FilterByPrice(float maxPrice)
+        {
+            var products = _db.Products.Where(p => p.Price <= maxPrice)
+                              .Select(p => new ProductVM
+                              {
+                                  Id = p.ID,
+                                  Name = p.Name,
+                                  Description = p.Description ?? "",
+                                  Link = _db.ProductImages.Where(pi => pi.ProductId == p.ID).Select(pi => pi.Link).ToList(),
+                                  Price = p.Price
+                              })
+                              .ToList();
+
+            return View("Index",products);
+        }
+
+        public IActionResult Detail(int pId)
+        {
+            var product = _db.Products.Where(p => p.ID == pId).FirstOrDefault();
+            return View(product);
         }
     }
 }
