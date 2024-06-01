d700fc3d241acbf18fd8fef3c1d63e06fff9ce83
dustinng04 <minhduc27025@gmail.com>
2024-06-01 16:26:23 +0700

    Add admin&staff layout

---
+++ Source/Website/HoaLacLaptopShop/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoaLacLaptopShop.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+
+    }
+}
