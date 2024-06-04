4705a17a46a846364d7b6b8bd89da0f63c4896d6
dustinng04 <minhduc27025@gmail.com>
2024-06-04 23:50:51 +0700

    Fix API tinh thanh & Fix checkout

--- Source/Website/HoaLacLaptopShop/Controllers/AdminController.cs
+++ Source/Website/HoaLacLaptopShop/Controllers/AdminController.cs
@@ -1,14 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HoaLacLaptopShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HoaLacLaptopShop.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly HoaLacLaptopShopContext _context;
+        
+        public AdminController(HoaLacLaptopShopContext context)
+        {
+            _context = context;
+        }
         public IActionResult Product()
         {
             return View();
         }
-
+        
+        public IActionResult Order()
+        {
+            var order = _context.Orders.Include(o => o.Buyer)
+                .Include(o => o.OrderDetails)
+                    .ThenInclude(oi => oi.Product).ToList();
+            return View(order);
+        }
 
     }
 }
