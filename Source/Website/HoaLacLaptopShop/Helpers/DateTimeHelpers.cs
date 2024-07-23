c7dbc5be9c2c51f15b8e3b0efad90d67a0f42c6f
thisis2838 <47449032+thisis2838@users.noreply.github.com>
2024-07-23 16:22:40 +0700

    NewsPosts: better time ago strings

---
+++ Source/Website/HoaLacLaptopShop/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,35 @@
+﻿namespace HoaLacLaptopShop.Helpers
+{
+	public static class DateTimeHelpers
+	{
+		public static string ToTimeFromNow(this DateTime dateTime)
+		{
+			var diff = DateTime.Now - dateTime;
+			string diffStr = diff >= TimeSpan.Zero ? "ago" : "ahead";
+
+			int value; string unit;
+			if (diff.TotalDays > 0)
+			{
+				value = (int)diff.TotalDays;
+				unit = "day";
+			}
+			else if (diff.TotalHours > 0)
+			{
+				value = (int)diff.TotalHours;
+				unit = "hour";
+			}
+			else if (diff.TotalMinutes > 0)
+			{
+				value = (int)diff.TotalMinutes;
+				unit = "minute";
+			}
+			else
+			{
+				value = (int)diff.TotalSeconds;
+				unit = "second";
+			}
+
+			return $"{value} {(value == 1 ? unit : (unit + 's'))} {diffStr}";
+		}
+	}
+}
